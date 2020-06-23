310d7352b28818f4b91a1564e0959181f3051cd7
adam <ajhanson22227@gmail.com>
2020-06-22 19:40:18 -0500

    Finished with the js portion. Added all functionality

--- script.js
+++ script.js
@@ -1,39 +1,92 @@
-function playRound(playerSelection, computerSelection){
 
+let computerChoices = ['rock', 'paper', 'scissors'];
+let computerSelection = "";
+
+let playerScoreText = document.getElementById('player-scoreID');
+let playerScore = 0;
+playerScoreText.innerHTML = playerScore;
+
+let computerScoreText = document.getElementById('computer-scoreID');
+let computerScore = 0;
+computerScoreText.innerHTML = computerScore;
+
+//main gameplay function
+function startRound(playerSelection){
+
+
+    computerSelection = getComputerChoice();
+    let roundStatus = getRoundWinner(playerSelection, computerSelection);
+    roundMessageScores(roundStatus, playerSelection, computerSelection);
+
+    checkForGameWinner();
+
+    console.log(`Player: ${playerSelection}\tComputer: ${computerSelection}`);
+}
+
+function getComputerChoice(){
+    var compSelector = Math.floor(Math.random() * 3);
+    return computerChoices[compSelector];
+}
+
+function getRoundWinner(playerSelection, computerSelection){
     if (playerSelection === computerSelection)
-        return "It's a tie";
+        return 0;
 
     if (playerSelection == "rock"){
         if (computerSelection == "paper")
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            return 2;
         else
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            return 1;
     }
     if (playerSelection == "paper"){
         if (computerSelection == "scissors")
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            return 2;
         else
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            return 1;
     }
     if (playerSelection == "scissors"){
         if (computerSelection == "rock")
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            return 2;
         else
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            return 1;
     }
-
 }
 
-let computerChoices = ['rock', 'paper', 'scissors'];
-
-let playerChoice = "";
-while (!computerChoices.includes(playerChoice)){
-    playerChoice = prompt("What is your choice? Rock Paper or Scissors");
-    playerChoice = playerChoice.toLocaleLowerCase();
+function roundMessageScores(roundStatus, playerSelection, computerSelection){
+    //tie
+    if(roundStatus == 0)
+        document.getElementById('announcements').innerHTML = `It's a tie`;
+    //player wins
+    else if (roundStatus == 1){
+        document.getElementById('announcements').innerHTML = `Your ${playerSelection} beats ${computerSelection}`;
+        console.log("Player Won");
+        playerScore++;
+        playerScoreText.innerHTML = playerScore;
+    }
+    //computer wins
+    else{
+        document.getElementById('announcements').innerHTML = `Computer's ${computerSelection} beats your ${playerSelection}`;
+        console.log("Computer Won");
+        computerScore++;
+        computerScoreText.innerHTML = computerScore;
+    }
 }
 
+function checkForGameWinner(){
+    if (playerScore == 5){
+        console.log('player wins the game');
+        document.getElementById('choices-container').innerHTML = "You Win";
+        document.getElementById('choices-container').style.display = "block";
+        document.getElementById('reset-button').style.display = "block";
+    }
+    if (computerScore == 5){
+        console.log('computer wins the game');
+        document.getElementById('choices-container').innerHTML = "Computer Wins";
+        document.getElementById('choices-container').style.display = "block";
+        document.getElementById('reset-button').style.display = "block";
+    }
+}
 
-var compSelector = Math.floor(Math.random() * 3);
-let computerChoice = computerChoices[compSelector];
-
-console.log(playRound(playerChoice, computerChoice));
+function reloadPage(){
+    window.location.reload();
+}
